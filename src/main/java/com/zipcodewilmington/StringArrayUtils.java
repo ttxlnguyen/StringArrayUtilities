680a57acceaf11951ad941aef30fc367afdc702c
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-07-04 15:56:33 -0400

    started

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,5 +1,10 @@
 package com.zipcodewilmington;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 /**
  * Created by leon on 1/29/18.
  */
@@ -9,7 +14,7 @@ public class StringArrayUtils {
      * @return first element of specified array
      */ // TODO
     public static String getFirstElement(String[] array) {
-        return array[0];
+        return array[0]; // Index 0
     }
 
     /**
@@ -17,7 +22,7 @@ public class StringArrayUtils {
      * @return second element in specified array
      */
     public static String getSecondElement(String[] array) {
-        return array[1];
+        return array[1]; // Index 1
     }
 
     /**
@@ -25,7 +30,7 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+        return array[array.length-1]; // Last element in array
     }
 
     /**
@@ -33,7 +38,7 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+        return array[array.length-2]; // Second to last element in array
     }
 
     /**
@@ -42,6 +47,11 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
+        for (int i = 0; i < array.length; i++) { // Iterates through an array
+            if (array[i].equals(value)) { // Checks to see if the array at index [i] is equal to value given using .equals for exact string
+                return true;
+            }
+        }
         return false;
     }
 
@@ -50,7 +60,12 @@ public class StringArrayUtils {
      * @return an array with identical contents in reverse order
      */ // TODO
     public static String[] reverse(String[] array) {
-        return null;
+        String[] arr = new String[array.length]; // Instantiates the array
+        for (int i = 0; i < array.length; i++){ // Iterates through the String[] array
+            arr[i] = array[array.length - 1 - i]; // Sets array reverse at index i to what the last element of String[] array
+            // -1 because arrays start at index 0. -i because it will gradually go through the indexes of String[] array in descending order.
+        }
+        return arr;
     }
 
     /**
@@ -58,7 +73,17 @@ public class StringArrayUtils {
      * @return true if the order of the array is the same backwards and forwards
      */ // TODO
     public static boolean isPalindromic(String[] array) {
-        return false;
+        String[] reverse = new String[array.length]; // Instantiates the array
+
+        for (int i = 0; i < array.length; i++) { // Iterates through the String[] array
+            reverse[i] = array[array.length - 1 - i]; // Sets array reverse at index i to what the last element of String[] array
+            // -1 because arrays start at index 0. -i because it will gradually go through the indexes of String[] array in descending order.
+
+            if (Arrays.equals(array, reverse)) { // The built-in command Array.equals checks if two arrays are equal.
+                return true; // If equal, returns true
+            }
+        }
+        return false; // In all other cases, returns false
     }
 
     /**
@@ -66,7 +91,8 @@ public class StringArrayUtils {
      * @return true if each letter in the alphabet has been used in the array
      */ // TODO
     public static boolean isPangramic(String[] array) {
-        return false;
+
+        return true;
     }
 
     /**
@@ -79,7 +105,7 @@ public class StringArrayUtils {
     }
 
     /**
-     * @param array         array of String objects
+     * @param array array of String objects
      * @param valueToRemove value to remove from array
      * @return array with identical contents excluding values of `value`
      */ // TODO
@@ -92,6 +118,7 @@ public class StringArrayUtils {
      * @return array of Strings with consecutive duplicates removes
      */ // TODO
     public static String[] removeConsecutiveDuplicates(String[] array) {
+
         return null;
     }
 
@@ -102,6 +129,4 @@ public class StringArrayUtils {
     public static String[] packConsecutiveDuplicates(String[] array) {
         return null;
     }
-
-
 }
